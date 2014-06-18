35dd8b3e15016db91741a25fda52383ee3f1e60d
wulfthor <thorwulf@hotmail.com>
2014-06-18 15:17:24 +0200

    shell script to parse xml-dump

---
+++ clean2MCM.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+input=$1
+itype=$2
+deflang=DA
+bagfile="/home/thw/tmp/mcm/bag-info.txt"
+export MYLANG=$deflang
+
+
+if [ $itype -gt 1 ]; then
+  perl -pi -e 's/&lt;/</g' $input
+  perl -pi -e 's/&gt;/>/g' $input
+  perl -pi -e 's/&#xE6;/æ/g' $input
+  perl -pi -e 's/&#xF8;/ø/g' $input
+  perl -pi -e 's/&#xE5;/å/g' $input
+  perl -pi -e 's/&#xD8;/Ø/g' $input
+  perl -pi -e 's/&#xE9;/é/g' $input
+fi
+
+
+
+if [ $itype -gt 2 ]; then
+  #perl -pi -e 's/\s{,3}<Title>(.*)<\/Title>/DC-Title: $deflang \1/' $input
+  #perl -pi -e 's/\s{,2}<Title>(.*)<\/Title>/DC-Title: \1/' $input
+  #perl -pi -e 's/<Title>(.*)<\/Title>/DC-Titlex: $ENV{MYLANG} \1/' $input
+  perl -pi -e 's/^\s{1,2}<Title>(.*)<\/Title>/DC-Title: \1/' $input
+  #perl -pi -e 's/<Title>(.*)<\/Title>/DC-Title: \1/' $input
+  perl -pi -e 's/<MCM.Data.DTO.ExtendedObjectInfo>/DC-SOURCE:/g' $input
+  perl -pi -e 's/<Abstract>(.*)<\/Abstract>/DC-Abstract: \1/g' $input
+  perl -pi -e 's/<Description>(.*)<\/Description>/DC-Description: \1/g' $input
+  perl -pi -e 's/<ObjectID>(.*)<\/ObjectID>/DC-Identifier: \1/g' $input
+  perl -pi -e 's/<Person Name=(.*) Role=(.*) \/>/DC-Creator: \2 \1/g' $input
+  perl -pi -e 's/<Person Name=(.*) Role=(.*) \/>/DC-Creator: \2 \1/g' $input
+  perl -pi -e 's/<Person Name=(.*) Role=(.*) \/>/DC-Creator: \2 \1/g' $input
+  perl -pi -e 's/<Person Name=(.*) Role=(.*) \/>/DC-Creator: \2 \1/g' $input
+  perl -pi -e 's/<Person Name=(.*) Role=(.*) \/>/DC-Creator: \2 \1/g' $input
+  #perl -pi -e 's/\// \& /g' $input
+  perl -pi -e 's/<DownloadURL>(.*)<\/DownloadURL>/DC-RELATION: \1/g' $input
+  perl -pi -e 's/"//g' $input
+fi
+
+#egrep "DC-" "$input" 
+#echo "DONE"
+egrep "DC-" "$input" | sed 's/^[ ]*//g' > $bagfile
+
+
+#grep DC- "$input" | sort | uniq | sed 's/^[ ]*//g' >> $bagfile
